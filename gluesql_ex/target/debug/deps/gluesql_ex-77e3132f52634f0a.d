4ee4c803efcee32ccb331f784c88fed104cb4b7d
jaeyoung0909 <jaeyoung0909@kaist.ac.kr>
2022-06-24 15:34:39 +0900

    init

---
+++ gluesql_ex/target/debug/deps/gluesql_ex-77e3132f52634f0a.d
@@ -0,0 +1,5 @@
+/Users/moonde/Documents/glue-sql-example/gluesql_ex/target/debug/deps/gluesql_ex-77e3132f52634f0a.rmeta: src/main.rs
+
+/Users/moonde/Documents/glue-sql-example/gluesql_ex/target/debug/deps/gluesql_ex-77e3132f52634f0a.d: src/main.rs
+
+src/main.rs:
---
+++ gluesql_ex/target/debug/deps/gluesql_ex-a3b307e79aa8da51.d
@@ -0,0 +1,5 @@
+/Users/moonde/Documents/glue-sql-example/gluesql_ex/target/debug/deps/gluesql_ex-a3b307e79aa8da51.rmeta: src/main.rs
+
+/Users/moonde/Documents/glue-sql-example/gluesql_ex/target/debug/deps/gluesql_ex-a3b307e79aa8da51.d: src/main.rs
+
+src/main.rs:
